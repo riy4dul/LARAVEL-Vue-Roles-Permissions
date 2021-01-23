cfa9e48cdf63611918210ae377e0521d06d2f8d2
Riyadul <starriyadul@gmail.com>
2021-01-23 18:05:20 +0600

    Loding screen

--- resources/js/app.js
+++ resources/js/app.js
@@ -28,7 +28,7 @@ Vue.filter("date", function(created){
 
  Vue.component('role', require('./components/role.vue').default);
  Vue.component('user', require('./components/user.vue').default);
-
+Vue.component('loading', require('./components/loading.vue').default);
 
 
 
